3e67a6803fc41d6fd868a1e8a74ff08f875ce8f6
vip2335 <vishalparmar84200@gmail.com>
2022-12-05 17:41:44 +0530

    comment file are deleted and all done wrt to rails guide

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,25 @@
 class ArticlesController < ApplicationController
+
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
   def index
-    # byebug
+
     @articles = Article.all
   end
 
   def show
+
     @article = Article.find(params[:id])
   end
 
   def new
+   
     @article = Article.new
   end
 
   def create
-    # byebug
+
     @article = Article.new(article_params)
 
     if @article.save
@@ -38,6 +44,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+
     @article = Article.find(params[:id])
     @article.destroy
 
@@ -47,6 +54,12 @@ class ArticlesController < ApplicationController
 
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body,:status)
     end
+end
+
+
+private
+def article_params
+  params.require(:article).permit(:title, :body, :status)
 end
